ac6f9ca9424c1b092d6f27b2fccdaac8ba56b6ba
hitman-stack <45466709+hitman-stack@users.noreply.github.com>
2021-01-17 16:03:12 +0530

    Initial project setup
    - Adding projoect sulution
    - Adding unit test solution
    - Adding approach in Read me file

---
+++ InventoryUpdater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inventory Updater!");
+        }
+    }
+}
